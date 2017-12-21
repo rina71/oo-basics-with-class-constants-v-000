06bcac91e174534a95abbba4f7d1211a69905d7c
Yurina Shin <yurinas@gmail.com>
2017-12-21 06:21:59 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -19,9 +19,9 @@ BRANDS = [ ]
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-end
+#   def brand=(brand)
+#     @brand = brand
+#     BRANDS << brand
+# end
 
 end
