ced61ece794f7f48679bdef21bd64336be38feeb
Yurina Shin <yurinas@gmail.com>
2017-12-21 06:25:29 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 BRANDS = [ ]
   def initialize(brand)
     @brand = brand
-    BRANDS << brand if !(BRANDS.include?(BRANDS))
+    BRANDS << brand if !(BRANDS.include?(brand))
   end
 # end
 #   def brand=(brand)
